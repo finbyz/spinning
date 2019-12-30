94bfbc56c7b102445c396f669982d189cc6dd7cd
Ubuntu <ubuntu@ip-172-31-16-97.ap-south-1.compute.internal>
2019-12-30 07:09:43 +0000

    Updated Gate entry exit

--- spinning/spinning/doctype/job_work_return/job_work_return.js
+++ spinning/spinning/doctype/job_work_return/job_work_return.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2019, FinByz Tech Pvt Ltd and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Job Work Return', {
-	// refresh: function(frm) {
 
-	// }
+frappe.ui.form.on('Job Work Return', {
+	refresh: function(frm) {
+		frappe.model.with_doc("BOM", frm.doc.bom_no, function() {
+			var os_doc = frappe.model.get_doc("BOM", frm.doc.bom_no)
+			//console.log("quantity",d.quantity);
+			$.each(os_doc.items, function(index, row){
+				let d = frm.add_child("items");
+				d.item_code = row.item_code;
+				//d.qty = flt(flt( os_doc.quantity * row.qty) / frm.doc.qty );
+				console.log("quantity",os_doc.quantity * row.qty);
+
+				// d.source_warehouse = frm.doc.default_source_warehouse;
+				//d.required_quantity = flt(flt(frm.doc.target_qty * row.quantity) / os_doc.total_qty);
+			})
+		});
+
+	}
 });
