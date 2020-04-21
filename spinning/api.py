c621aa79da8a1517ac26ebfe49cf66a7f4b70694
Finbyz <info@finbyz.com>
2020-04-21 05:34:27 +0000

    gate exit changes

--- spinning/api.py
+++ spinning/api.py
@@ -103,7 +103,7 @@ def get_package_details(batch_no,to_date):
 	
 @frappe.whitelist()
 def before_naming(self, method):
-	if not self.amended_from:
+	if not hasattr(self,'amended_from'):
 		
 		date = self.get("transaction_date") or self.get("posting_date") or getdate()
 		fiscal = get_fiscal(date)
